771932e5072b0c29c14f0db2f62b4febf2bc7af4
aiex718 <aiex718g@gmail.com>
2023-03-27 00:11:21 +0800

    USART use bitwise flag instead of callback pending queue to prevent duplicate callback. Change callback regist function.

--- MDK/CCTV3_F407_MDK_REV200/User/bsp/sys/callback.h
+++ MDK/CCTV3_F407_MDK_REV200/User/bsp/sys/callback.h
@@ -9,15 +9,8 @@ typedef enum {
     INVOKE_IN_IRQ=1,
 }Callback_IRqCfg_t;
 
-//CallbackIT is always invoked, no matter in interrupt or task/main thread
-__BSP_STRUCT_ALIGN typedef struct
-{
-    void (*func)(void* sender,void* param);
-    void *param;
-}CallbackIRq_t;
-
-//Callback_t is (selectable) invoked in interrupt or delayed to task/main thread
-//due to callback issuer, it might not be invoked as IRQCFG settingm
+//Callback_t is (selectable) invoked in interrupt or postponed to task/main thread
+//due to callback issuer, config INVOKE_IN_IRQ is not always fulfilled
 __BSP_STRUCT_ALIGN typedef struct
 {
     void (*func)(void* sender,void* param);
@@ -27,13 +20,34 @@ __BSP_STRUCT_ALIGN typedef struct
 
 __BUFFER_DECL(Callback_t*,CallbackP_t)
 
+//Macro as function
 #define Callback_Invoke(sender,cb) do{ \
     if((cb)!=NULL) (cb)->func((sender),(cb)->param); }while(0)   
-
-// #define Callback_InvokeOrQueue_Auto(sender,cb,queue) \
-//     Callback_InvokeOrQueue((sender),(cb),(queue),SysCtrl_IsThreadInIRq())
+#define Callback_TryInvoke_IRq(sender,cb) \
+    Callback_TryInvoke((sender),(cb),INVOKE_IN_IRQ)
 #define Callback_InvokeOrQueue_IRq(sender,cb,queue) \
     Callback_InvokeOrQueue((sender),(cb),(queue),INVOKE_IN_IRQ)
+// #define Callback_InvokeOrQueue_Auto(sender,cb,queue) \
+//     Callback_InvokeOrQueue((sender),(cb),(queue),SysCtrl_IsThreadInIRq())
+
+/**
+ * @brief try invoke callback irqcfg match cb->IRqCfg
+ * @param sender: sender of callback
+ * @param cb: callback to invoke
+ * @return true if callback is invoked, false if not invoked
+ */
+__STATIC_INLINE bool Callback_TryInvoke(void *sender, Callback_t *cb,
+    Callback_IRqCfg_t irqcfg)
+{
+    if(cb->IRqCfg == irqcfg)
+    {
+        Callback_Invoke(sender,cb);
+        return true;
+    }
+    else
+        return false;
+}
+
 /**
  * @brief invoke callback or queue it, invoked directly if queue is full
  * @details Normally we only queue callback in IRQ, which qualvient to 
@@ -44,15 +58,13 @@ __BUFFER_DECL(Callback_t*,CallbackP_t)
  * @param sender: sender of callback
  * @param queue: queue to store callback
  * @param cb: callback to invoke or queue
- * @return true if the callback is invoked, false if the callback is queued or null
+ * @return true if callback is invoked, false if the callback is queued 
  */
-__STATIC_INLINE bool Callback_InvokeOrQueue(void *sender,
-    Callback_t *cb, Buffer_CallbackP_t *queue,Callback_IRqCfg_t irqcfg)
+__STATIC_INLINE bool Callback_InvokeOrQueue(void *sender, Callback_t *cb,
+    Buffer_CallbackP_t *queue,Callback_IRqCfg_t irqcfg)
 {
-    if(cb!=NULL && (
-            cb->IRqCfg == irqcfg || 
-            Buffer_Queue_Push_CallbackP_t(queue,cb)==false)
-        )
+    if( cb->IRqCfg == irqcfg || queue==NULL ||
+        Buffer_Queue_Push_CallbackP_t(queue,cb)==false )
     {
         Callback_Invoke(sender,cb);
         return true;
