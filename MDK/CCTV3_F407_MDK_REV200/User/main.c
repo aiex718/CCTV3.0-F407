7ada60419aba609af68ee1c0eb524218458dbf7c
aiex718 <aiex718g@gmail.com>
2023-03-27 00:11:53 +0800

    Edit main to adapt new callback function.

--- MDK/CCTV3_F407_MDK_REV200/User/main.c
+++ MDK/CCTV3_F407_MDK_REV200/User/main.c
@@ -9,45 +9,77 @@
 
 //Normally, you should not directly print stuff in ISR callback,
 //because our buffer is not thread safe, this is just for test purpose
-void Tx_Empty_Callback(void *sender,void* para)
+void Tx_Empty(void *sender,void* para)
 {
 	HAL_GPIO_TogglePin(LED_STAT_pin);
 }
-void Rx_ThrsReach_Callback(void *sender,void* para)
+
+void Rx_ThrsReach(void *sender,void* para)
 { 
-	uint8_t ch;
+	//because rx_buf could receive data anytime,
+	//so we should snap buf size or use usart->USART_Rx_Threshold as rx count
+	//instead of while(Buffer_Queue_Pop_uint8_t)
 	HAL_USART_t* usart = (HAL_USART_t*)sender;
-	printf("Rx_ThrsReach_Callback,in IRq %d\n",SysCtrl_IsThreadInIRq()); 
-	printf("len %d, content:",Buffer_Queue_GetSize(usart->USART_Rx_Buf));
-	while(Buffer_Queue_Pop_uint8_t(usart->USART_Rx_Buf,&ch))
-		printf("%c",ch);
-	printf("\n");
+	Buffer_uint8_t *rx_queue=usart->USART_Rx_Buf;
+	uint16_t cnt=usart->USART_Rx_Threshold,i=0;
+	uint8_t rx_data[Debug_Serial_Rx_Buffer_Size]={0};
+	while (i<cnt)
+	{
+		Buffer_Queue_Pop_uint8_t(rx_queue,rx_data+i);
+		i++;
+	}
+	
+	printf("Rx_ThrsReach_Callback,in IRq %d ,len:%d ,content: %s\n",
+		SysCtrl_IsThreadInIRq(),cnt,rx_data);
 }
-void Rx_Timeout_Callback(void *sender,void* para)
+
+void Rx_Timeout(void *sender,void* para)
 {	
 	uint8_t ch;
+	bool IsRxStreamEnabled=HAL_USART_IsRxStreamEnabled(Debug_Usart3);
 	HAL_USART_t* usart = (HAL_USART_t*)sender;
-	printf("Rx_Timeout_Callback,in IRq %d ,",SysCtrl_IsThreadInIRq());
-	printf("len %d, content:",Buffer_Queue_GetSize(usart->USART_Rx_Buf));
+	Buffer_uint8_t *tx_queue=usart->USART_Tx_Buf;
+
+	while (HAL_USART_IsTransmitting(usart));// wait until tx finished
+	Buffer_Clear(tx_queue);
+	tx_queue->w_ptr+=sprintf(tx_queue->w_ptr,(IsRxStreamEnabled ? "Rx from stream, ":"Rx from dma, "));
+	tx_queue->w_ptr+=sprintf(tx_queue->w_ptr,"Rx_Timeout_Callback,in IRq %d ,",SysCtrl_IsThreadInIRq());
+	tx_queue->w_ptr+=sprintf(tx_queue->w_ptr,"len %d, content:",Buffer_Queue_GetSize(usart->USART_Rx_Buf));
 	while(Buffer_Queue_Pop_uint8_t(usart->USART_Rx_Buf,&ch))
-		printf("%c",ch);
-	printf("\n");
+		Buffer_Queue_Push_uint8_t(tx_queue,ch);
+	tx_queue->w_ptr+=sprintf(tx_queue->w_ptr,"\n");
+
+	//switch rx between DMA and non-DMA mode
+	if(IsRxStreamEnabled)
+	{
+		HAL_USART_RxStreamCmd(Debug_Usart3,false);
+		HAL_USART_DmaRead(Debug_Usart3,0);
+	}
+	else
+	{
+		HAL_USART_RxStreamCmd(Debug_Usart3,true);
+	}
+	
+	HAL_USART_DmaWrite(usart);
 }
 //Config rx threshold to larger than rx_queue max capacity to test these callback
-void Rx_Dropped_Callback(void *sender,void* para)
+//Becareful these callback is executed in ISR, and printf will block until tx finished
+//so if tx buffer is full, it will block the ISR, thread will hang forever
+void Rx_Dropped(void *sender,void* para)
 {
 	printf("Rx_Dropped_Callback,in IRq %d\n",SysCtrl_IsThreadInIRq());
 }
-void Rx_Full_Callback(void *sender,void* para)
+void Rx_Full(void *sender,void* para)
 {
 	printf("Rx_Full_Callback,in IRq %d\n",SysCtrl_IsThreadInIRq());
 }
 
-Callback_t USART_Tx_Empty_Callback = {Tx_Empty_Callback,NULL,INVOKE_IN_IRQ};
-Callback_t USART_Rx_ThrsReach_Callback = {Rx_ThrsReach_Callback,NULL,INVOKE_IN_TASK};
-Callback_t USART_Rx_Timeout_Callback = {Rx_Timeout_Callback,NULL,INVOKE_IN_TASK};
-CallbackIRq_t USART_Rx_Dropped_Callback = {Rx_Dropped_Callback,NULL};
-CallbackIRq_t USART_Rx_Full_Callback = {Rx_Full_Callback,NULL};
+Callback_t Tx_Empty_Callback = {Tx_Empty,NULL,INVOKE_IN_IRQ};
+Callback_t Rx_ThrsReach_Callback = {Rx_ThrsReach,NULL,INVOKE_IN_TASK};
+Callback_t Rx_Timeout_Callback = {Rx_Timeout,NULL,INVOKE_IN_TASK};
+Callback_t Rx_Dropped_Callback = {Rx_Dropped,NULL,INVOKE_IN_IRQ};
+Callback_t Rx_Full_Callback = {Rx_Full,NULL,INVOKE_IN_IRQ};
+
 Timer_t blinkTimer;
 char ch='a';
 int main(void)
@@ -58,40 +90,49 @@ int main(void)
 	HAL_GPIO_InitPin(LED_STAT_pin);
 	HAL_GPIO_InitPin(LED_Load_pin);
 	HAL_USART_Init(Debug_Usart3);
-	Debug_Usart3->USART_Tx_Empty_Callback = &USART_Tx_Empty_Callback;
-	Debug_Usart3->USART_Rx_ThrsReach_Callback = &USART_Rx_ThrsReach_Callback;
-	Debug_Usart3->USART_Rx_Timeout_Callback = &USART_Rx_Timeout_Callback;
-	Debug_Usart3->USART_Rx_Dropped_Callback = &USART_Rx_Dropped_Callback;
-	Debug_Usart3->USART_Rx_Full_Callback = &USART_Rx_Full_Callback;
+	HAL_GPIO_WritePin(LED_STAT_pin,0);
+
+	HAL_USART_SetCallback(Debug_Usart3,USART_CALLBACK_TX_EMPTY,&Tx_Empty_Callback);
+	HAL_USART_SetCallback(Debug_Usart3,USART_CALLBACK_RX_THRSHOLD,&Rx_ThrsReach_Callback);
+	HAL_USART_SetCallback(Debug_Usart3,USART_CALLBACK_RX_TIMEOUT,&Rx_Timeout_Callback);
+	HAL_USART_SetCallback(Debug_Usart3,USART_CALLBACK_IRQ_RX_DROPPED,&Rx_Dropped_Callback);
+	HAL_USART_SetCallback(Debug_Usart3,USART_CALLBACK_IRQ_RX_FULL,&Rx_Full_Callback);
 
 	HAL_USART_Cmd(Debug_Usart3,true);
-	HAL_USART_RxStreamCmd(Debug_Usart3,true);
+	HAL_USART_DmaRead(Debug_Usart3,0);
 
-	printf("hello world!\n");
+	printf("Hello World from tx stream!\n");
+	while (HAL_USART_IsTransmitting(Debug_Usart3));// wait until tx finished
+
+	{
+		Buffer_uint8_t *tx_queue=Debug_Usart3->USART_Tx_Buf;
+		Buffer_Clear(tx_queue);
+		uint16_t w_len = sprintf(tx_queue->w_ptr,"Hello World from DMA! \n");
+		tx_queue->w_ptr += w_len;
+		HAL_USART_DmaWrite(Debug_Usart3);
+	}
+	
+	//tx dma ongoing, this should fail
+	HAL_USART_Write(Debug_Usart3,
+		"2nd Hello World from tx stream, you should not see me!\n",
+		strlen("2nd Hello World from tx stream, you should not see me!\n"));
+
+	//this will block thread until DMA tx finished
+	printf("3rd Hello World from tx stream!\n");
+	
 	Timer_Init(&blinkTimer,1000);
-	HAL_GPIO_WritePin(LED_STAT_pin,0);
 
 	while(1)
 	{
 		HAL_USART_Service(Debug_Usart3);
-
-		//stress test tx queue
-		//could overwhalm your PC if it's a potato :P
-		printf("%c",ch++);
-		if(ch>'z') 
-		{	
-			ch='a';
-			printf("\n");
-		} 
 
 		//blink Load LED
 		if(Timer_IsElapsed(&blinkTimer))
 		{
 			HAL_GPIO_TogglePin(LED_Load_pin);
 			Timer_Reset(&blinkTimer);
-			//printf("%d:Wkup pin %d\n",Systime_Get(),HAL_GPIO_ReadPin(Button_Wkup_pin));
+			printf("%d:Wkup pin %d\n",Systime_Get(),HAL_GPIO_ReadPin(Button_Wkup_pin));
 		}
-		
 	}
 }
 
