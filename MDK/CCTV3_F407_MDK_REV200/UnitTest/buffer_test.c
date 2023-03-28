ad0897821f1ab474008dad52f497d6e3a7c18670
aiex718 <aiex718g@gmail.com>
2023-03-28 15:50:35 +0800

    Update buffer_test unit test to able to test IndexOf

--- MDK/CCTV3_F407_MDK_REV200/UnitTest/buffer_test.c
+++ MDK/CCTV3_F407_MDK_REV200/UnitTest/buffer_test.c
@@ -16,76 +16,81 @@
 #define TEST_QUEUE 1
 
 #ifdef TEST_QUEUE
-#define PUSH_ARRAY_FUNC Buffer_Queue_PushArray_uint8_t
-#define POP_ARRAY_FUNC  Buffer_Queue_PopArray_uint8_t
+#define PUSH_ARRAY_FUNC Buffer_Queue_PushArray_uint32_t
+#define POP_ARRAY_FUNC  Buffer_Queue_PopArray_uint32_t
+#define INDEXOF_FUNC    Buffer_Queue_IndexOf_uint32_t
 #define GETSIZE_FUNC    Buffer_Queue_GetSize
 #endif
 
 #ifdef TEST_STACK
-#define PUSH_ARRAY_FUNC Buffer_Stack_PushArray_uint8_t
-#define POP_ARRAY_FUNC  Buffer_Stack_PopArray_uint8_t
+#define PUSH_ARRAY_FUNC Buffer_Stack_PushArray_uint32_t
+#define POP_ARRAY_FUNC  Buffer_Stack_PopArray_uint32_t
 #define GETSIZE_FUNC    Buffer_Stack_GetSize
 #endif
-
-void reverseArray(int *arr, int size) {
-    int *reverseArr = (int *) malloc(size * sizeof(int));
-    int i, j;
-
-    // Reverse the array
-    for (i = size - 1, j = 0; i >= 0; i--, j++) {
-        reverseArr[j] = arr[i];
-    }
-
-    // Copy the reversed array back to the original array
-    for (i = 0; i < size; i++) {
-        arr[i] = reverseArr[i];
-    }
-
-    // Free dynamically allocated memory
-    free(reverseArr);
-}
-
 
 int main() {
     srand(time(NULL)); // Seed the random number generator
 
-    Buffer_uint8_t queue;
-    uint8_t data[MAX_DATA_LEN];
+    bool error=false;
+    Buffer_uint32_t queue;
+    uint32_t data[MAX_DATA_LEN];
 
     // Initialize the queue
-    uint8_t buffer[BUFFER_SIZE]; // Example buffer with 256 bytes of storage
+    uint32_t buffer[BUFFER_SIZE]; // Example buffer with 256 bytes of storage
     queue.buf_ptr = buffer;
-    queue.len = sizeof(buffer);
+    queue.len = sizeof(buffer)/sizeof(buffer[0]);
     queue.r_ptr = queue.buf_ptr;
     queue.w_ptr = queue.buf_ptr;
 
     int num_iterations = ITERATION_COUNT; // Example number of iterations
-    uint8_t* all_data_pushed = (uint8_t*) malloc(sizeof(uint8_t)*MAX_DATA_LEN*num_iterations);
-    uint8_t* all_data_popped = (uint8_t*) malloc(sizeof(uint8_t)*MAX_DATA_LEN*num_iterations);
+    uint32_t* all_data_pushed = (uint32_t*) malloc(sizeof(uint32_t)*MAX_DATA_LEN*num_iterations);
+    uint32_t* all_data_popped = (uint32_t*) malloc(sizeof(uint32_t)*MAX_DATA_LEN*num_iterations);
     int total_push_cnt = 0;
     int total_pop_cnt = 0;
 
     for (int i = 0; i < num_iterations; i++) 
     {
-        // First push and pop sequence
+        // Push and pop sequence
         printf("Push and pop sequence:\n");
         int push_len1 = rand() % MAX_DATA_LEN + 1; // Random length of data to push
         for (int j = 0; j < push_len1; j++) {
-            data[j] = rand() % 256; // Example data range from 0 to 255
+            data[j] = rand();
         }
+        int queue_left_cnt = GETSIZE_FUNC(&queue);
+
         int actual_push_cnt = PUSH_ARRAY_FUNC(&queue, data, push_len1);
         printf("Try pushing %d , actual pushed %d bytes: \t",push_len1, actual_push_cnt);
         for (int j = 0; j < actual_push_cnt; j++) {
             all_data_pushed[total_push_cnt++] = data[j];
-            printf("%02x ", data[j]);
+            printf("%x ", data[j]);
         }
+        
+        //check index of 
+        for (int j = 0; j < actual_push_cnt; j++) {
+            uint16_t idx;
+            if(INDEXOF_FUNC(&queue,data[j],&idx))
+            {   
+                if(idx != queue_left_cnt+j)
+                {
+                    error=true;
+                    printf("\nIndex not match, of %x is %d\n",data[j],idx);
+                }
+            }
+            else
+            {
+                error=true;
+                printf("\nIndex not found, of %x\n",data[j]);
+            }
+        }
+        
+
         printf("\n");
         int pop_len1 = rand() % push_len1 + 1; // Random length of data to pop
         int actual_pop_cnt =POP_ARRAY_FUNC(&queue, data, pop_len1);
         printf("Try popping %d ,actual pop %d bytes: \t",pop_len1, actual_pop_cnt);
         for (int j = 0; j < actual_pop_cnt; j++) {
             all_data_popped[total_pop_cnt++] = data[j];
-            printf("%02x ", data[j]);
+            printf("%x ", data[j]);
         }
         printf("\nRemaning %ld\n",GETSIZE_FUNC(&queue));
     }
@@ -98,10 +103,10 @@ int main() {
     for (int j = 0; j < pop_len_all; j++) 
     {
         all_data_popped[total_pop_cnt++] = data[j];
-        printf("%02x ", data[j]);
+        printf("%x ", data[j]);
     }
+    printf("\n");
 
-    bool error=false;
     if(total_push_cnt!=total_pop_cnt)
     {
         printf("Error: Data pushed len and popped len are not equal\n");
