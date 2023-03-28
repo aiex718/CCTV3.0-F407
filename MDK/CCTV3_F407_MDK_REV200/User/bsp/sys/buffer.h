bf0ad9d96780b1a77192c16b73004939b6f80536
aiex718 <aiex718g@gmail.com>
2023-03-28 15:45:14 +0800

    Buffer add Queue_IndexOf amd Queue_Remove fucntion.

--- MDK/CCTV3_F407_MDK_REV200/User/bsp/sys/buffer.h
+++ MDK/CCTV3_F407_MDK_REV200/User/bsp/sys/buffer.h
@@ -22,103 +22,120 @@ __BSP_STRUCT_ALIGN typedef struct           \
     __type          *r_ptr;                 \
     __type          *w_ptr;                 \
 }Buffer_##__name;                           \
-/* Queue generic funstions  */                                                      \
-static BUFFER_STORAGE_LEN_T Buffer_Queue_PushArray_##__name                         \
-(Buffer_##__name *self, __type *data, BUFFER_STORAGE_LEN_T len)                     \
-{                                                                                   \
-    BUFFER_STORAGE_LEN_T w_count=0;                                                 \
-    if(data==NULL) return 0;                                                        \
-    while(len && Buffer_Queue_IsFull(self)==false)                                  \
-    {                                                                               \
-        BUFFER_STORAGE_LEN_T w_len;                                                 \
-        if(self->w_ptr >= self->r_ptr)                                              \
-            w_len = self->buf_ptr + self->len - self->w_ptr -                       \
-                                            (self->r_ptr == self->buf_ptr ? 1 : 0); \
-        else                                                                        \
-            w_len = self->r_ptr - self->w_ptr - 1;                                  \
-                                                                                    \
-        w_len = BSP_MIN(w_len,len);                                                 \
-        BSP_MEMCPY(self->w_ptr,data,w_len);                                         \
-        data+=w_len;                                                                \
-        __Buffer_Queue_MovePtr(self,self->w_ptr,w_len);                             \
-                                                                                    \
-        len-=w_len;                                                                 \
-        w_count+=w_len;                                                             \
-    }                                                                               \
-                                                                                    \
-    return w_count;                                                                 \
-}                                                                                   \
-static BUFFER_STORAGE_LEN_T Buffer_Queue_PopArray_##__name                          \
-(Buffer_##__name *self, __type *data, BUFFER_STORAGE_LEN_T len)                     \
-{                                                                                   \
-    BUFFER_STORAGE_LEN_T r_count=0;                                                 \
-    if(data==NULL) return 0;                                                        \
-    while(len && Buffer_Queue_IsEmpty(self)==false)                                 \
-    {                                                                               \
-        BUFFER_STORAGE_LEN_T r_len;                                                 \
-        if(self->r_ptr >= self->w_ptr)                                              \
-            r_len = self->buf_ptr + self->len - self->r_ptr;                        \
-        else                                                                        \
-            r_len = self->w_ptr - self->r_ptr;                                      \
-                                                                                    \
-        r_len = BSP_MIN(r_len,len);                                                 \
-        BSP_MEMCPY(data,self->r_ptr,r_len);                                         \
-        data+=r_len;                                                                \
-        __Buffer_Queue_MovePtr(self,self->r_ptr,r_len);                             \
-                                                                                    \
-        len-=r_len;                                                                 \
-        r_count+=r_len;                                                             \
-    }                                                                               \
-    return r_count;                                                                 \
-}                                                                                   \
-__STATIC_INLINE bool Buffer_Queue_Push_##__name                                     \
-(Buffer_##__name *self, __type x)                                                   \
-{                                                                                   \
-    if(Buffer_Queue_IsFull(self))                                                   \
-        return false;                                                               \
-    Buffer_Queue_QuickPush(self,x);                                                 \
-    return true;                                                                    \
-}                                                                                   \
-__STATIC_INLINE bool Buffer_Queue_Pop_##__name                                      \
-(Buffer_##__name *self, __type *o)                                                  \
-{                                                                                   \
-    if(Buffer_Queue_IsEmpty(self)) return false;                                    \
-    Buffer_Queue_QuickPop(self,*o);                                                 \
-    return true;                                                                    \
-}                                                                                   \
-/* Stack generic funstions  */                                                      \
-static BUFFER_STORAGE_LEN_T Buffer_Stack_PushArray_##__name                         \
-(Buffer_##__name *self, __type *data, BUFFER_STORAGE_LEN_T len)                     \
-{                                                                                   \
-    BUFFER_STORAGE_LEN_T cnt=0;                                                     \
-    if(data==NULL) return 0;                                                        \
-    cnt=Buffer_Stack_GetSize(self);                                                 \
-    while(len-- && Buffer_Stack_IsFull(self)==false)                                \
-        Buffer_Stack_QuickPush(self,*data++);                                       \
-    return Buffer_Stack_GetSize(self)-cnt;                                          \
-}                                                                                   \
-static BUFFER_STORAGE_LEN_T Buffer_Stack_PopArray_##__name                          \
-(Buffer_##__name *self, __type *data, BUFFER_STORAGE_LEN_T len)                     \
-{                                                                                   \
-    BUFFER_STORAGE_LEN_T cnt=0;                                                     \
-    if(data==NULL) return 0;                                                        \
-    cnt=Buffer_Stack_GetSize(self);                                                 \
-    while(len-- && Buffer_Stack_IsEmpty(self)==false)                               \
-        Buffer_Stack_QuickPop(self,*data++);                                        \
-    return cnt-Buffer_Stack_GetSize(self);                                          \
-}                                                                                   \
-__STATIC_INLINE bool Buffer_Stack_Push_##__name                                     \
-(Buffer_##__name *self, __type x)                                                   \
-{                                                                                   \
-    if(Buffer_Stack_IsFull(self))   return false;                                   \
-    Buffer_Stack_QuickPush(self,x); return true;                                    \
-}                                                                                   \
-__STATIC_INLINE bool Buffer_Stack_Pop_##__name                                      \
-(Buffer_##__name *self, __type *o)                                                  \
-{                                                                                   \
-    if(Buffer_Stack_IsEmpty(self))  return false;                                   \
-    Buffer_Stack_QuickPop(self,*o); return true;                                    \
+/* Queue generic funstions  */                                      \
+static BUFFER_STORAGE_LEN_T Buffer_Queue_PushArray_##__name         \
+(Buffer_##__name *self, __type *data, BUFFER_STORAGE_LEN_T len)     \
+{                                                                   \
+    BUFFER_STORAGE_LEN_T w_count=0;                                 \
+    if(data==NULL) return 0;                                        \
+    while(len && Buffer_Queue_IsFull(self)==false)                  \
+    {                                                               \
+        BUFFER_STORAGE_LEN_T w_len;                                 \
+        if(self->w_ptr >= self->r_ptr)                              \
+            w_len = self->buf_ptr + self->len - self->w_ptr -       \
+                    (self->r_ptr == self->buf_ptr ? 1 : 0);         \
+        else                                                        \
+            w_len = self->r_ptr - self->w_ptr - 1;                  \
+                                                                    \
+        w_len = BSP_MIN(w_len,len);                                 \
+        BSP_MEMCPY(self->w_ptr,data,w_len*sizeof(__type));          \
+        data+=w_len;                                                \
+        __Buffer_Queue_MovePtr(self,self->w_ptr,w_len);             \
+                                                                    \
+        len-=w_len;                                                 \
+        w_count+=w_len;                                             \
+    }                                                               \
+    return w_count;                                                 \
+}                                                                   \
+static BUFFER_STORAGE_LEN_T Buffer_Queue_PopArray_##__name          \
+(Buffer_##__name *self, __type *data, BUFFER_STORAGE_LEN_T len)     \
+{                                                                   \
+    BUFFER_STORAGE_LEN_T r_count=0;                                 \
+    if(data==NULL) return 0;                                        \
+    while(len && Buffer_Queue_IsEmpty(self)==false)                 \
+    {                                                               \
+        BUFFER_STORAGE_LEN_T r_len;                                 \
+        if(self->r_ptr >= self->w_ptr)                              \
+            r_len = self->buf_ptr + self->len - self->r_ptr;        \
+        else                                                        \
+            r_len = self->w_ptr - self->r_ptr;                      \
+                                                                    \
+        r_len = BSP_MIN(r_len,len);                                 \
+        BSP_MEMCPY(data,self->r_ptr,r_len*sizeof(__type));          \
+        data+=r_len;                                                \
+        __Buffer_Queue_MovePtr(self,self->r_ptr,r_len);             \
+                                                                    \
+        len-=r_len;                                                 \
+        r_count+=r_len;                                             \
+    }                                                               \
+    return r_count;                                                 \
+}                                                                   \
+__STATIC_INLINE bool Buffer_Queue_Push_##__name                     \
+(Buffer_##__name *self, __type x)                                   \
+{                                                                   \
+    if(Buffer_Queue_IsFull(self))                                   \
+        return false;                                               \
+    Buffer_Queue_QuickPush(self,x);                                 \
+    return true;                                                    \
+}                                                                   \
+__STATIC_INLINE bool Buffer_Queue_Pop_##__name                      \
+(Buffer_##__name *self, __type *o)                                  \
+{                                                                   \
+    if(Buffer_Queue_IsEmpty(self)) return false;                    \
+    Buffer_Queue_QuickPop(self,*o);                                 \
+    return true;                                                    \
+}                                                                   \
+__STATIC_INLINE bool Buffer_Queue_IndexOf_##__name                  \
+(Buffer_##__name *self, __type data, BUFFER_STORAGE_LEN_T *idx_out) \
+{                                                                   \
+    BUFFER_STORAGE_LEN_T idx = 0;                                   \
+    __type *ptr = self->r_ptr;                                      \
+    while(ptr != self->w_ptr)                                       \
+    {                                                               \
+        if(*ptr == data)                                            \
+        {                                                           \
+            *idx_out = idx;                                         \
+            return true;                                            \
+        }                                                           \
+        __Buffer_Queue_IncPtr(self,ptr);                            \
+        idx++;                                                      \
+    }                                                               \
+    return false;                                                   \
+}                                                                   \
+/* Stack generic funstions  */                                      \
+static BUFFER_STORAGE_LEN_T Buffer_Stack_PushArray_##__name         \
+(Buffer_##__name *self, __type *data, BUFFER_STORAGE_LEN_T len)     \
+{                                                                   \
+    BUFFER_STORAGE_LEN_T cnt=0;                                     \
+    if(data==NULL) return 0;                                        \
+    cnt=Buffer_Stack_GetSize(self);                                 \
+    while(len-- && Buffer_Stack_IsFull(self)==false)                \
+        Buffer_Stack_QuickPush(self,*data++);                       \
+    return Buffer_Stack_GetSize(self)-cnt;                          \
+}                                                                   \
+static BUFFER_STORAGE_LEN_T Buffer_Stack_PopArray_##__name          \
+(Buffer_##__name *self, __type *data, BUFFER_STORAGE_LEN_T len)     \
+{                                                                   \
+    BUFFER_STORAGE_LEN_T cnt=0;                                     \
+    if(data==NULL) return 0;                                        \
+    cnt=Buffer_Stack_GetSize(self);                                 \
+    while(len-- && Buffer_Stack_IsEmpty(self)==false)               \
+        Buffer_Stack_QuickPop(self,*data++);                        \
+    return cnt-Buffer_Stack_GetSize(self);                          \
+}                                                                   \
+__STATIC_INLINE bool Buffer_Stack_Push_##__name                     \
+(Buffer_##__name *self, __type x)                                   \
+{                                                                   \
+    if(Buffer_Stack_IsFull(self))   return false;                   \
+    Buffer_Stack_QuickPush(self,x); return true;                    \
+}                                                                   \
+__STATIC_INLINE bool Buffer_Stack_Pop_##__name                      \
+(Buffer_##__name *self, __type *o)                                  \
+{                                                                   \
+    if(Buffer_Stack_IsEmpty(self))  return false;                   \
+    Buffer_Stack_QuickPop(self,*o); return true;                    \
 }
+
 
 //Queue and stack mode shared functions
 #define Buffer_GetStorageSize(self) ((self)->len)
@@ -132,10 +149,13 @@ __STATIC_INLINE bool Buffer_Stack_Pop_##__name                                  
     (((self)->r_ptr == (self)->buf_ptr) && ((self)->w_ptr == ((self)->buf_ptr + (self)->len - 1))))
 #define Buffer_Queue_GetSize(self) \
     (((self)->w_ptr >= (self)->r_ptr)?((self)->w_ptr - (self)->r_ptr):((self)->w_ptr - (self)->r_ptr + (self)->len))
-#define Buffer_Queue_QuickPush(self,x) \
-    do{ *((self)->w_ptr)=(x);__Buffer_Queue_IncPtr((self),(self)->w_ptr);}while(0)
-#define Buffer_Queue_QuickPop(self,o) \
-    do{(o)= *((self)->r_ptr);__Buffer_Queue_IncPtr((self),(self)->r_ptr);}while(0)
+#define Buffer_Queue_QuickPush(self,x) do{  \
+    *((self)->w_ptr)=(x);__Buffer_Queue_IncPtr((self),(self)->w_ptr);  }while(0)
+#define Buffer_Queue_QuickPop(self,o)  do{  \
+    (o)= *((self)->r_ptr);__Buffer_Queue_IncPtr((self),(self)->r_ptr); }while(0)
+#define Buffer_Queue_Remove(self,cnt)  do{  \
+    BUFFER_STORAGE_LEN_T i=cnt;             \
+    while(i--) __Buffer_Queue_IncPtr((self),(self)->r_ptr); }while(0)
 #define Buffer_Queue_GetMaxCapacity(self)   (Buffer_GetStorageSize(self) - 1)
 #define Buffer_Queue_GetCapacity(self)      (Buffer_Queue_GetMaxCapacity(self)-Buffer_Queue_GetSize(self))
 
