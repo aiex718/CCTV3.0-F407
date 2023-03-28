2deba4f2662fef1e7df5f8b373e50af39b24ffc1
aiex718 <aiex718g@gmail.com>
2023-03-28 16:49:41 +0800

    First BSP released. trimmed main, remove test project.

--- MDK/CCTV3_F407_MDK_REV200/User/main.c
+++ MDK/CCTV3_F407_MDK_REV200/User/main.c
@@ -5,10 +5,9 @@
 #include "bsp/sys/timer.h"
 #include "bsp/sys/sysctrl.h"
 #include "bsp/sys/dbg_serial.h"
+#include "stdio.h"
 
 Timer_t blinkTimer;
-char ch='a';
-bool StressTestTx=false;
 int main(void)
 {
 	//SystemInit() is inside system_stm32f4xx.c
@@ -18,66 +17,24 @@ int main(void)
 	DBG_Serial_Init(DBG_Serial);
 	DBG_Serial_AttachUSART(DBG_Serial,Debug_Usart3);
 	HAL_USART_Cmd(Debug_Usart3,true);
-
+	printf("Built at " __DATE__ " " __TIME__ " ,Booting...\n");
 
 	HAL_GPIO_InitPin(Button_Wkup_pin);
 	HAL_GPIO_InitPin(LED_STAT_pin);
 	HAL_GPIO_InitPin(LED_Load_pin);
 	HAL_GPIO_WritePin(LED_STAT_pin,0);
 	
-	printf("Boot\n");
 
 	
 	Timer_Init(&blinkTimer,1000);
 	while(1)
 	{
-		uint8_t rxcmd[16]={0};
+		uint8_t rxcmd[Debug_Serial_Rx_Buffer_Size]={0};
 		if(DBG_Serial_ReadLine(DBG_Serial,rxcmd,16))
 		{
 			if(strcmp((char*)rxcmd,"hello")==0)
-			{
 				printf("hello there\n");
-			}
-			else if(strcmp((char*)rxcmd,"stress")==0)
-			{
-				StressTestTx=true;
-				printf("Stress test started\n");
-			}
-			else if(strcmp((char*)rxcmd,"stop")==0)
-			{
-				StressTestTx=false;
-				printf("Stress test stopped\n");
-			}
-			else if(strcmp((char*)rxcmd,"reset")==0)
-			{
-				printf("Resetting...\n");
-				SysCtrl_Reset();
-			}
-			else if(strcmp((char*)rxcmd,"help")==0)
-			{
-				printf("Commands:\n");
-				printf("stress - start stress test\n");
-				printf("stop - stop stress test\n");
-				printf("reset - reset the board\n");
-				printf("hello - echo hello\n");
-				printf("help - print this help\n");
-			}
-			else
-			{
-				printf("Unknown command:%s\n",rxcmd);
-			}
 		}
-		if(StressTestTx)
-		{
-			printf("%c",ch++);
-			if(ch>'z') 
-			{	
-				ch='a';
-				printf("\n %d:",Systime_Get());
-			} 
-		}
-
-
 
 		HAL_USART_Service(Debug_Usart3);
 		DBG_Serial_Service(DBG_Serial);
@@ -87,7 +44,6 @@ int main(void)
 		{
 			HAL_GPIO_TogglePin(LED_Load_pin);
 			Timer_Reset(&blinkTimer);
-			if(StressTestTx==false)
 				printf("%d:Wkup pin %d\n",Systime_Get(),HAL_GPIO_ReadPin(Button_Wkup_pin));
 		}
 	}
