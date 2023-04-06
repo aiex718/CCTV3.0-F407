1a1f9fab34a5e247bc1813123c8bf138e2742423
aiex718 <aiex718g@gmail.com>
2023-04-06 20:38:09 +0800

    Edit attach logic, now only set pointer, move init part to Init function.

--- MDK/CCTV3_F407_MDK_REV200/User/device/flashlight.c
+++ MDK/CCTV3_F407_MDK_REV200/User/device/flashlight.c
@@ -3,20 +3,26 @@
 void Device_FlashLight_Init(Device_FlashLight_t *self)
 {
     HAL_GPIO_InitPin(self->FlashLight_GPIO_pin);
+    if(self->FlashLight_Timer_PWM)
+    {
+        HAL_Timer_PWM_InitChannel(self->FlashLight_Timer_PWM,
+        self->FlashLight_Timer_PWM_Channel);
+        Device_FlashLight_SetBrightness(self,self->FlashLight_Brightness);
+    }
 }
 
 void Device_FlashLight_Attach(Device_FlashLight_t *self,const HAL_Timer_PWM_t *timer_PWM)
 {
     self->FlashLight_Timer_PWM = timer_PWM;
-    HAL_Timer_PWM_InitChannel(self->FlashLight_Timer_PWM,
-        self->FlashLight_Timer_PWM_Channel);
-    Device_FlashLight_SetBrightness(self,self->FlashLight_Brightness);
 }
 
 void Device_FlashLight_Cmd(Device_FlashLight_t *self,bool en)
 {
-    HAL_Timer_PWM_ChannelCmd(self->FlashLight_Timer_PWM,
-        self->FlashLight_Timer_PWM_Channel,en);
+    if(self->FlashLight_Timer_PWM)
+    {
+        HAL_Timer_PWM_ChannelCmd(self->FlashLight_Timer_PWM,
+            self->FlashLight_Timer_PWM_Channel,en);
+    }
 }
 
 void Device_FlashLight_SetBrightness(Device_FlashLight_t *self,uint8_t brightness)
