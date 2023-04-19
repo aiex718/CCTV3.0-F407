30ba8f31cf3e0dd6c0a8829fe2c982de8540b576
aiex718 <aiex718g@gmail.com>
2023-04-19 17:20:28 +0800

    change allow preemption to bool flag instead of #define ,add comment for concurrent_queue

--- MDK/CCTV3_F407_MDK_REV200/User/bsp/sys/concurrent_queue.c
+++ MDK/CCTV3_F407_MDK_REV200/User/bsp/sys/concurrent_queue.c
@@ -8,6 +8,33 @@
 //equal to x%d, but faster when d is power of 2
 #define __CONQUEUE_MOD_FAST(x,d) (x&((d)-1))
 
+/*
+If task A is preempted by another task B during pushing data,
+
+  - With preempt disabled:
+    Task B can't push data to queue directly, it'll have to wait 
+    until task A finish pushing.
+    
+    Pop from queue is always allowed.
+
+  - With preempt enabled:
+    Task B can push data to queue directly without waiting task A.
+    This mechanism allow task B to be a ISR, and can boost 
+    performance in scenarios with multiple push tasks.
+
+    But when preempt is enabled, it's not allowed to pop from 
+    queue until all data is pushed. 
+    We have to let task A finish pushing before we can pop any data.
+    i.e. We can preempt a push task, but we can't starve it.
+         It'll cause queue pop fail during starved time.
+
+Conclusion:
+    In single-consumer scenario, 
+    enable preempt has better performance.
+    
+    In multi-consumer scenario, 
+    disable preempt has better performance.
+*/
 void Concurrent_Queue_Init(__CONCURRENT_QUEUE_T *self, 
                             CONCURRENT_QUEUE_STORAGE_T* buf, 
                             CONCURRENT_QUEUE_STORAGE_LEN_T buf_len  )
@@ -39,7 +66,7 @@ bool Concurrent_Queue_TryPush(__CONCURRENT_QUEUE_T *self,
         
         if (snap_w_idx-snap_r_idx>=self->mem_len)//is Full
             return false;
-        else if(Atomic_Cmpxchg(CONCURRENT_QUEUE_INDEX_T,
+        else if(Atomic_Cmpxchg_Type(CONCURRENT_QUEUE_INDEX_T,
                                 &self->w_idx,(snap_w_idx+1),
                                 snap_w_idx) ==snap_w_idx)
             break;
@@ -47,17 +74,18 @@ bool Concurrent_Queue_TryPush(__CONCURRENT_QUEUE_T *self,
     
     self->mem[__CONQUEUE_MOD_FAST(snap_w_idx,self->mem_len)] = val;
 
-#if CONCURRENT_QUEUE_ALLOW_PREEMPT
-    Atomic_Increment(CONCURRENT_QUEUE_INDEX_T,&self->l_idx);
-#else
-    do
+    if(self->allow_push_preempt)
+        Atomic_Increment_Type(CONCURRENT_QUEUE_INDEX_T,&self->l_idx);
+    else
     {
-        if(Atomic_Cmpxchg(CONCURRENT_QUEUE_INDEX_T,
-                            &self->l_idx,(snap_w_idx+1),
-                            snap_w_idx)==snap_w_idx)
-            break;
-    } while (true);
-#endif
+        do
+        {
+            if(Atomic_Cmpxchg_Type(CONCURRENT_QUEUE_INDEX_T,
+                                &self->l_idx,(snap_w_idx+1),
+                                snap_w_idx)==snap_w_idx)
+                break;
+        } while (true);
+    }
     return true;
 }
 
@@ -72,18 +100,15 @@ bool Concurrent_Queue_TryPop(__CONCURRENT_QUEUE_T *self,
         snap_l_idx = self->l_idx;
         snap_w_idx = self->w_idx;
 
-#if CONCURRENT_QUEUE_ALLOW_PREEMPT
-        if ((snap_l_idx)!=(snap_w_idx))//queue is writing
+        if (self->allow_push_preempt && (snap_l_idx)!=(snap_w_idx))//queue is writing
             return false;
-        else 
-#endif
-        if (snap_r_idx==snap_l_idx)//empty
+        else if (snap_r_idx==snap_l_idx)//empty
             return false;
         else
         {
             *out = self->mem[__CONQUEUE_MOD_FAST(snap_r_idx,self->mem_len)];
 
-            if (Atomic_Cmpxchg(CONCURRENT_QUEUE_INDEX_T,
+            if (Atomic_Cmpxchg_Type(CONCURRENT_QUEUE_INDEX_T,
                                 &self->r_idx,(snap_r_idx+1),
                                 snap_r_idx)==snap_r_idx)
                 return true;
