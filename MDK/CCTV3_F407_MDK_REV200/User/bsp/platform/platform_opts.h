0a5e55acd35ae44ec53ae70bbdd15a1b8a4257c2
aiex718 <aiex718g@gmail.com>
2023-04-23 23:01:12 +0800

    Fix Default_SysFault_Loop.

--- MDK/CCTV3_F407_MDK_REV200/User/bsp/platform/platform_opts.h
+++ MDK/CCTV3_F407_MDK_REV200/User/bsp/platform/platform_opts.h
@@ -2,5 +2,5 @@
 #define __PLATFORM_OPTS_H__
 
 #define DBG_SERIAL_ENABLE_DMA 1
-
+#define DBG_SERIAL_SHOW_INFO 1
 #endif
--- MDK/CCTV3_F407_MDK_REV200/User/bsp/sys/system_its.c
+++ MDK/CCTV3_F407_MDK_REV200/User/bsp/sys/system_its.c
@@ -1,60 +1,46 @@
 #include "bsp/sys/systime.h"
-#include "bsp/sys/systimer.h"
 #include "bsp/sys/dbg_serial.h"
-#include "bsp/hal/gpio.h"
-#include "bsp/platform/periph_list.h"
 
-__STATIC_INLINE void Default_SysFault_Loop(const char *msg,uint16_t blink_period,uint16_t print_period)
+__STATIC_INLINE void Default_SysFault_Loop(const char *msg,uint32_t delay)
 {
-    SysTimer_t tmr_blink,tmr_print;
-    SysTimer_Init(&tmr_blink,blink_period);
-    SysTimer_Init(&tmr_print,print_period);
-    
     DBG_Serial_SafeMode(DBG_Serial,true);
-    printf("%s",msg);
+    __IO uint32_t cnt;
     
     while (1)
     {
-        if(SysTimer_IsElapsed(&tmr_blink))
-        {
-            SysTimer_Reset(&tmr_blink);
-            HAL_GPIO_TogglePin(Periph_LED_STAT_pin);
-        }
-        if(SysTimer_IsElapsed(&tmr_print))
-        {
-            SysTimer_Reset(&tmr_print);
-            printf("%s",msg);
-        }
+        cnt=delay;
+        printf("%s",msg);
+        while(cnt--);
     }
 }
 
 void NMI_Handler(void)
 {
-    Default_SysFault_Loop("NMI_Handler",1000,3000);
+    Default_SysFault_Loop("NMI_Handler\n",0xfffff);
 }
 
 void HardFault_Handler(void)
 {
     /* Go to infinite loop when Hard Fault exception occurs */
-    Default_SysFault_Loop("HardFault_Handler",200,3000);
+    Default_SysFault_Loop("HardFault_Handler\n",0xfffff);
 }
 
 void MemManage_Handler(void)
 {
     /* Go to infinite loop when Memory Manage exception occurs */
-    Default_SysFault_Loop("MemManage_Handler",200,3000);
+    Default_SysFault_Loop("MemManage_Handler\n",0xfffff);
 }
 
 void BusFault_Handler(void)
 {
     /* Go to infinite loop when Bus Fault exception occurs */
-    Default_SysFault_Loop("BusFault_Handler",200,3000);
+    Default_SysFault_Loop("BusFault_Handler\n",0xfffff);
 }
 
 void UsageFault_Handler(void)
 {
     /* Go to infinite loop when Usage Fault exception occurs */
-    Default_SysFault_Loop("UsageFault_Handler",200,3000);
+    Default_SysFault_Loop("UsageFault_Handler\n",0xfffff);
 }
 
 void DebugMon_Handler(void)
