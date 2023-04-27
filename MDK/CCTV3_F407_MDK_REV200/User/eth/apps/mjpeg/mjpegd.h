19ea4ac3cd6f4dc37a623c3c31f1c6faa3bca85a
aiex718 <aiex718g@gmail.com>
2023-04-28 04:44:07 +0800

    remove forward decl in mjpegd.h

--- MDK/CCTV3_F407_MDK_REV200/User/eth/apps/mjpeg/mjpegd.h
+++ MDK/CCTV3_F407_MDK_REV200/User/eth/apps/mjpeg/mjpegd.h
@@ -6,26 +6,19 @@
 #include "lwip/err.h"
 #include "lwip/tcp.h"
 
-
-//forward declaration
-typedef struct Mjpegd_FrameBuf_struct Mjpegd_FrameBuf_t;
-typedef struct Mjpegd_Camera_struct Mjpegd_Camera_t;
-typedef struct ClientState_struct ClientState_t;
-typedef struct Mjpegd_Frame_struct Mjpegd_Frame_t;
-
 typedef struct {
     u16_t Port;
-    Mjpegd_FrameBuf_t *FrameBuf;
-    Mjpegd_Camera_t *Camera;
+    struct Mjpegd_FrameBuf_struct *FrameBuf;
+    struct Mjpegd_Camera_struct *Camera;
 
     //private, dont use
     struct tcp_pcb *_main_pcb;
-    ClientState_t *_clients_list;
+    struct ClientState_struct *_clients_list;
     u8_t _client_count;
     u8_t _stream_count;
     //pending frame and timer for fps,
     //these variables are shared between main thread and camera thread/ISR
-    Mjpegd_Frame_t *_pending_frame;
+    struct Mjpegd_Frame_struct *_pending_frame;
     MJPEGD_SYSTIME_T _fps_timer;
     u16_t _fps_counter;
     u16_t _drop_counter;
