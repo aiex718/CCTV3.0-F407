13ed1bbbe565da7665e54074fd4b50fb4d943026
aiex718 <aiex718g@gmail.com>
2023-04-29 15:53:43 +0800

    same as previous

--- MDK/CCTV3_F407_MDK_REV200/User/bsp/platform/app/app_mjpegd.c
+++ MDK/CCTV3_F407_MDK_REV200/User/bsp/platform/app/app_mjpegd.c
@@ -8,7 +8,6 @@
 Mjpegd_t App_Mjpegd_Inst={
     .Port = 8080,
     .FramePool = __VAR_CAST_VAR(Mjpegd_FramePool_t){
-        ._frames_len = MJPEGD_FRAMEPOOL_LEN,
         ._frames = __VAR_ARRAY_CAST_VAR(Mjpegd_Frame_t,MJPEGD_FRAMEPOOL_LEN){
             0
         },
