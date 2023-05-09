3f886bf4473834571c47cafd804cce18869d046a
aiex718 <aiex718g@gmail.com>
2023-05-10 05:10:41 +0800

    Impl NetTime config

--- MDK/CCTV3_F407_MDK_REV200/User/app/nettime/nettime.h
+++ MDK/CCTV3_F407_MDK_REV200/User/app/nettime/nettime.h
@@ -3,11 +3,25 @@
 
 #include "bsp/platform/platform_defs.h"
 
+
+typedef struct NetTime_ConfigFile_s
+{
+    uint8_t NetTime_Enable;
+    uint8_t __padding[3];
+    char NetTime_SNTP_Server[64];
+}NetTime_ConfigFile_t;
+
+
 typedef struct NetTime_s{
-    const char* NTP_Server;
-} NetTime_t;
+    bool NetTime_Enable;
+    const char* NetTime_SNTP_Server;
+}NetTime_t;
 
 void NetTime_Init(NetTime_t *self);
+void NetTime_ConfigSet(NetTime_t *self,const NetTime_ConfigFile_t *config);
+void NetTime_ConfigExport(const NetTime_t *self,NetTime_ConfigFile_t *config);
+bool NetTime_IsConfigValid(NetTime_t *self,const NetTime_ConfigFile_t *config);
+
 void NetTime_Sntp_Poll_Callback(uint32_t unix_epoch_sec);
 
 #endif
