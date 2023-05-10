05256686c6930fc69fb9201cc76066bff1f384c0
aiex718 <aiex718g@gmail.com>
2023-05-10 18:26:50 +0800

    Add back deleted file.

--- MDK/CCTV3_F407_MDK_REV200/User/bsp/platform/periph/periph_list.h
+++ MDK/CCTV3_F407_MDK_REV200/User/bsp/platform/periph/periph_list.h
@@ -3,6 +3,8 @@
 
 #include "bsp/platform/periph/peri_dbgserial.h"
 #include "bsp/platform/periph/peri_gpio.h"
+#include "bsp/platform/periph/peri_rng.h"
+#include "bsp/platform/periph/peri_uniqueid.h"
 
 
 #endif
