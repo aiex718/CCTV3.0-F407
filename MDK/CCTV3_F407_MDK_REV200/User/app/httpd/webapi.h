4a27ad43dcd409a28fe914f955487d2f8346b258
aiex718 <aiex718g@gmail.com>
2023-05-11 09:24:03 +0800

    Move unnecessary code to .c

--- MDK/CCTV3_F407_MDK_REV200/User/app/httpd/webapi.h
+++ MDK/CCTV3_F407_MDK_REV200/User/app/httpd/webapi.h
@@ -3,22 +3,10 @@
 
 #include "bsp/platform/platform_defs.h"
 
-#include "lwip/apps/fs.h"//struct fs_file
-
 #ifndef WEBAPI_RESPONSE_BUFFER_LEN
     #define WEBAPI_RESPONSE_BUFFER_LEN 256
 #endif
 
 extern const char* const Webapi_Enter_Point;
 
-typedef enum {
-    WEBAPI_OK = 0,
-    WEBAPI_ERR_NOTFOUND,
-} WebApi_Result_t;
-
-WebApi_Result_t httpd_api_uptime(struct fs_file *file, \
-    const char* uri, int iNumParams,char **pcParam, char **pcValue);
-
 #endif
-
-
