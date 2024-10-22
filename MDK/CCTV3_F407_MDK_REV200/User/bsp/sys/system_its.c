22dbeb45371eac6fc1504742e83b52e23f166f60
aiex718 <aiex718g@gmail.com>
2024-10-23 03:11:33 +0800

    Edit Dump_Stack function for more debug info

--- MDK/CCTV3_F407_MDK_REV200/User/bsp/sys/system_its.c
+++ MDK/CCTV3_F407_MDK_REV200/User/bsp/sys/system_its.c
@@ -9,39 +9,28 @@
 
 #if USE_DEFAULT_SYSFAULT_HANDLER
 
-void Dump_Stack(unsigned int *hardfault_args, unsigned int lr_value ,char* msg)
+void Dump_Stack(unsigned int *msp_psp,unsigned int *regs, unsigned int lr_value ,char* msg)
 {
-    unsigned long stacked_r0;
-    unsigned long stacked_r1;
-    unsigned long stacked_r2;
-    unsigned long stacked_r3; 
-    unsigned long stacked_r12;
-    unsigned long stacked_lr;
-    unsigned long stacked_pc;
-    unsigned long stacked_psr;   
- 
-    stacked_r0 =  ((unsigned long)hardfault_args[0]);
-    stacked_r1 =  ((unsigned long)hardfault_args[1]);
-    stacked_r2 =  ((unsigned long)hardfault_args[2]);
-    stacked_r3 =  ((unsigned long)hardfault_args[3]);
-    stacked_r12 = ((unsigned long)hardfault_args[4]);
-    stacked_lr =  ((unsigned long)hardfault_args[5]);
-    stacked_pc =  ((unsigned long)hardfault_args[6]);
-    stacked_psr = ((unsigned long)hardfault_args[7]);
-
     DBG_Serial_SafeMode(Peri_DBG_Serial,true);
 
-    
     DBG_PRINTF("%s\n",msg);
-    DBG_PRINTF("Stack dump at MSP\\PSP 0x%08x\n",hardfault_args);
-    DBG_PRINTF(" R0  = 0x%08x\n",  stacked_r0);
-    DBG_PRINTF(" R1  = 0x%08x\n",  stacked_r1);
-    DBG_PRINTF(" R2  = 0x%08x\n",  stacked_r2);
-    DBG_PRINTF(" R3  = 0x%08x\n",  stacked_r3);
-    DBG_PRINTF(" R12 = 0x%08x\n", stacked_r12);
-    DBG_PRINTF(" LR  = 0x%08x\n",  stacked_lr);
-    DBG_PRINTF(" PC  = 0x%08x\n",  stacked_pc);
-    DBG_PRINTF(" PSR = 0x%08x\n", stacked_psr);    
+    DBG_PRINTF("Stack dump at %s 0x%08x\n",lr_value&0x04?"PSP":"MSP",msp_psp);
+    DBG_PRINTF(" R0  = 0x%08x\n",(unsigned long)msp_psp[0]);
+    DBG_PRINTF(" R1  = 0x%08x\n",(unsigned long)msp_psp[1]);
+    DBG_PRINTF(" R2  = 0x%08x\n",(unsigned long)msp_psp[2]);
+    DBG_PRINTF(" R3  = 0x%08x\n",(unsigned long)msp_psp[3]);
+    DBG_PRINTF(" R4  = 0x%08x\n",(unsigned long)regs[0]);
+    DBG_PRINTF(" R5  = 0x%08x\n",(unsigned long)regs[1]);
+    DBG_PRINTF(" R6  = 0x%08x\n",(unsigned long)regs[2]);
+    DBG_PRINTF(" R7  = 0x%08x\n",(unsigned long)regs[3]);
+    DBG_PRINTF(" R8  = 0x%08x\n",(unsigned long)regs[4]);
+    DBG_PRINTF(" R9  = 0x%08x\n",(unsigned long)regs[5]);
+    DBG_PRINTF(" R10 = 0x%08x\n",(unsigned long)regs[6]);
+    DBG_PRINTF(" R11 = 0x%08x\n",(unsigned long)regs[7]);
+    DBG_PRINTF(" R12 = 0x%08x\n",(unsigned long)msp_psp[4]);
+    DBG_PRINTF(" LR  = 0x%08x\n",(unsigned long)msp_psp[5]);
+    DBG_PRINTF(" PC  = 0x%08x\n",(unsigned long)msp_psp[6]);
+    DBG_PRINTF(" PSR = 0x%08x\n",(unsigned long)msp_psp[7]);    
     DBG_PRINTF(" LR/EXC_RETURN = 0x%08x\n", lr_value);
     //to jump to this function, msp must move forward 4 word=16 bytes,
     //if set breakpoint here, actual msp when exception occurs is  
@@ -72,14 +61,20 @@ __asm void HardFault_Handler(void)
     //put the value of the PSP into the r0 register, as first argument for the C handler
     mrsne  r0, psp
 
-    //put LR value into the r1 register, as second argument for the C handler
-    mov    r1, lr
+    //store r4-r11 and lr value to stack
+    push  {r4-r11}
+    //put address that store r4-r11 to r1, as second argument for the C handler
+    mrseq  r1, msp
+
+    //put LR value into the r2 register, as third argument for the C handler
+    mov    r2, lr
 
     //padding to make sure the length of this function is 4 bytes
-    nop    
+    //nop    
 
-    //put message to r2, as third argument for the C handler
-    ldr    r2, =__cpp(HardFaultMsg)
+    //put message to r3, as 4th argument for the C handler
+    ldr    r3, =__cpp(HardFaultMsg)
+
     //Branch to the C handler
     b      __cpp(Dump_Stack)
 }
